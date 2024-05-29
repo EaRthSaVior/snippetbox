4ff3ecd7b27265b7ccc47a88ffdb6aea12cffc42
Teoh Soon Ann <101308101+EaRthSaVior@users.noreply.github.com>
2024-05-30 01:09:36 +0800

    improve routing, form and stateful http

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -4,19 +4,26 @@ import (
 	"net/http"
 	"path/filepath"
 
+	"github.com/julienschmidt/httprouter"
 	"github.com/justinas/alice"
 )
 
 func (app *application) routes() http.Handler {
-	mux := http.NewServeMux()
-	fileServer := http.FileServer(neuteredFileSystem{http.Dir("./ui/static/")})
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet/view", app.snippetView)
-	mux.HandleFunc("/snippet/create", app.snippetCreate)
+	router := httprouter.New()
+	router.NotFound = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		app.notFound(w)
+	})
 
+	fileServer := http.FileServer(neuteredFileSystem{http.Dir("./ui/static/")})
+	router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static", fileServer))
+	dynamic := alice.New(app.sessionManager.LoadAndSave)
+
+	router.Handler(http.MethodGet, "/", dynamic.ThenFunc(app.home))
+	router.Handler(http.MethodGet, "/snippet/view/:id", dynamic.ThenFunc(app.snippetView))
+	router.Handler(http.MethodGet, "/snippet/create", dynamic.ThenFunc(app.snippetCreate))
+	router.Handler(http.MethodPost, "/snippet/create", dynamic.ThenFunc(app.snippetCreatePost))
 	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
-	return standard.Then(mux)
+	return standard.Then(router)
 }
 
 type neuteredFileSystem struct {
